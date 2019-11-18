06cf672864c1ae088c3c09c3f9c09605a1d0dcb4
ReeseMM <rmhemery2002@gmail.com>
2019-11-18 12:12:46 -0500

    mep

--- PartnerApp/ViewController.swift
+++ PartnerApp/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func ButtonTapped(_ sender: UIButton)
+    
     {
     
     }
