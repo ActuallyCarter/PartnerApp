d1610d19b09927132d2f95038bf7f1ff1623249f
Carter Shoemaker <actuallycarter@gmail.com>
2019-11-20 12:13:16 -0500

    working counter

--- PartnerApp/ViewControllerM.swift
+++ PartnerApp/ViewControllerM.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewControllerM: UIViewController {
 
+    @IBOutlet weak var imageTile1: UIImageView!
+    
+    @IBOutlet weak var imageTile2: UIImageView!
+    
+    @IBOutlet weak var imageTile3: UIImageView!
+    
+    @IBOutlet weak var imageTile4: UIImageView!
+    
+    @IBOutlet weak var imageTile5: UIImageView!
+    
+    @IBOutlet weak var imageTile6: UIImageView!
+    
+    @IBOutlet weak var imageTile7: UIImageView!
+    
+    @IBOutlet weak var imageTile8: UIImageView!
+    
+    @IBOutlet weak var imageTile9: UIImageView!
+    
+    @IBOutlet weak var imageTile10: UIImageView!
+    
+    @IBOutlet weak var imageTile11: UIImageView!
+    
+    @IBOutlet weak var imageTile12: UIImageView!
+    
+    @IBOutlet weak var imageTile13: UIImageView!
+    
+    @IBOutlet weak var imageTile14: UIImageView!
+    
+    @IBOutlet weak var imageTile16: UIImageView!
+    @IBOutlet weak var imageTile15: UIImageView!
+    
     @IBOutlet weak var Tile1: UIButton!
     
     @IBOutlet weak var Tile2: UIButton!
@@ -38,6 +69,7 @@ class ViewControllerM: UIViewController {
     @IBOutlet weak var Tile15: UIButton!
     @IBOutlet weak var Tile16: UIButton!
     var Names = ["Pete Dunaghy", "", "","","","Chris Walter","Ian Althouse",""]
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -46,15 +78,37 @@ class ViewControllerM: UIViewController {
     
     @IBAction func tilePressed(_ sender: UIButton) {
         
+        print("pressed")
+        
+        if sender.alpha == CGFloat(1.0) && counter < 2{
+        sender.alpha = 0.02
+        counter = counter + 1
         print("the word")
-        
-        if sender.alpha == CGFloat(1.0){
-        sender.alpha = 0.0
         }
-        else if sender.alpha == CGFloat(0.0) {
-            sender.alpha = 1.0
+        else if counter == 2{
+           setToDefault()
+            counter = 0
         }
         
+    }
+    func setToDefault() {
+        Tile1.alpha = 1.0
+        Tile2.alpha = 1.0
+        Tile3.alpha = 1.0
+        Tile4.alpha = 1.0
+        Tile5.alpha = 1.0
+        Tile6.alpha = 1.0
+        Tile7.alpha = 1.0
+        Tile8.alpha = 1.0
+        Tile9.alpha = 1.0
+        Tile10.alpha = 1.0
+        Tile11.alpha = 1.0
+        Tile12.alpha = 1.0
+        Tile13.alpha = 1.0
+        Tile14.alpha = 1.0
+        Tile15.alpha = 1.0
+        Tile16.alpha = 1.0
+
     }
     
     /*
