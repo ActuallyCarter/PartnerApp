afda280031473dd0c9292c125763599ffe690f03
Carter Shoemaker <actuallycarter@gmail.com>
2019-11-20 10:56:30 -0500

    szca

--- PartnerApp/ViewControllerM.swift
+++ PartnerApp/ViewControllerM.swift
@@ -10,13 +10,48 @@ import UIKit
 
 class ViewControllerM: UIViewController {
 
+    @IBOutlet weak var Tile1: UIButton!
+    
+    @IBOutlet weak var Tile2: UIButton!
+    
+    @IBOutlet weak var Tile3: UIButton!
+    @IBOutlet weak var Tile4: UIButton!
+    
+    @IBOutlet weak var Tile5: UIButton!
+    
+    @IBOutlet weak var Tile6: UIButton!
+    
+    @IBOutlet weak var Tile7: UIButton!
+    
+    @IBOutlet weak var Tile8: UIButton!
+    
+    @IBOutlet weak var Tile9: UIButton!
+    @IBOutlet weak var Tile10: UIButton!
+    
+    @IBOutlet weak var Tile11: UIButton!
+    
+    @IBOutlet weak var Tile12: UIButton!
+    
+    @IBOutlet weak var Tile13: UIButton!
+    @IBOutlet weak var Tile14: UIButton!
+    
+    @IBOutlet weak var Tile15: UIButton!
+    @IBOutlet weak var Tile16: UIButton!
+    var Names = ["Pete Dunaghy", "", "","","","Chris Walter","Ian Althouse",""]
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // for mem game use two layers of views, set alphas
     }
     
-
+    @IBAction func tilePressed(_ sender: UIButton) {
+        
+        
+        
+        
+        
+    }
+    
     /*
     // MARK: - Navigation
 
