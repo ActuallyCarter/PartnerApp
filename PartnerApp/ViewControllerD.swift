8e2810fbecd15a24735bb240d6bc0b7305206005
ReeseMM <rmhemery2002@gmail.com>
2019-11-20 11:00:45 -0500

    m

--- PartnerApp/ViewControllerD.swift
+++ PartnerApp/ViewControllerD.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewControllerD: UIViewController {
-
+    let teachers = ["Walter", "Althouse", "Jackson"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +17,5 @@ class ViewControllerD: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
