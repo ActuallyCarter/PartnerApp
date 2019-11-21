dd31b6b9bb3be45584ea8939e138580c3ffe2ce9
ReeseMM <rmhemery2002@gmail.com>
2019-11-21 11:57:29 -0500

    m

--- PartnerApp/ViewControllerD.swift
+++ PartnerApp/ViewControllerD.swift
@@ -22,7 +22,15 @@ class ViewControllerD: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        Button1.titleLabel?.text = "\(Names[0])"
+        
+        Button1.setTitle("\(Names[0])", for: .normal)
+        Button2.setTitle("\(Names[1])", for: .normal)
+        Button3.setTitle("\(Names[2])", for: .normal)
+        Button4.setTitle("\(Names[3])", for: .normal)
+        Button5.setTitle("\(Names[4])", for: .normal)
+        Button6.setTitle("\(Names[5])", for: .normal)
+        Button7.setTitle("\(Names[6])", for: .normal)
+        Button8.setTitle("\(Names[7])", for: .normal)
     }
     
 
