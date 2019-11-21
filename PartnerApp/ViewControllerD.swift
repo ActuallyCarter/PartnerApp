8100162da2bcc2e9bd478f308230209baf662d11
ReeseMM <rmhemery2002@gmail.com>
2019-11-21 11:44:08 -0500

    m

--- PartnerApp/ViewControllerD.swift
+++ PartnerApp/ViewControllerD.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class ViewControllerD: UIViewController {
     let Names = ["Pete Dunaghy", "Michal Fien", "Meghan Gerber","Megan Malligan","Victoria Pollard","Chris Walter","Ian Althouse","Jeffrey Jackson"]
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    @IBOutlet weak var Button4: UIButton!
+    @IBOutlet weak var Button5: UIButton!
+    @IBOutlet weak var Button6: UIButton!
+    @IBOutlet weak var Button7: UIButton!
+    @IBOutlet weak var Button8: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        Button1.titleLabel?.text = "\(Names[0])"
     }
     
 
