d1afd67ee6f17a7dac083a51af2c20220329623f
ReeseMM <rmhemery2002@gmail.com>
2019-11-25 11:40:53 -0500

    m

--- PartnerApp/ViewControllerD.swift
+++ PartnerApp/ViewControllerD.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewControllerD: UIViewController {
     let Names = ["Pete Dunaghy", "Michal Fein", "Meghan Gerber","Megan Malligan","Victoria Pollard","Chris Walter","Ian Althouse","Jeffrey Jackson"]
-    let Stats = ["Name: Pete Dunaghy\nYears At Haverford High: 400\nRole In School: Trendy Principal", "Name: Michal Fein\nYears At Haverford High: 10\nRole In School: Digital Musician", "Name: Meghan Gerber\nYears At Haverford High: 12\nRole In School: Digital Musician"]
+    let Stats = ["Name: Pete Dunaghy\nYears At Haverford High: 400\nRole In School: Trendy Principal", "Name: Michal Fein\nYears At Haverford High: 10\nRole In School: Digital Musician", "Name: Meghan Gerber\nYears At Haverford High: 12\nRole In School: History Guru", "Name: Megan Malligan\nYears In Haverford High: @\nRole In School: Health Informant", "Name: Victoria Pollard\n Years At Haverford High: <_>\nRole In School: Biology Planner", "Name: Chris Walter\nYears At Haverfor High: 00110010010\nRole In School: Mathmatic Programer", "Name: Ian Althouse\nYears At Haverford High: u\nRole In School: Master Editor", "Name: Jeffrey Jackson\nYears At Haverford High: 30ish\nRole In School: Numeric Athlete"]
     @IBOutlet weak var Button1: UIButton!
     @IBOutlet weak var Button2: UIButton!
     @IBOutlet weak var Button3: UIButton!
